11f4457257e2aee3f5530ffb1e5468ae5187ef77
Agent_RBY_ <mrget112@gmail.com>
2022-07-13 13:25:48 +0300

    Add guest status
    Took 51 minutes

--- src/guest/guest.dto.ts
+++ src/guest/guest.dto.ts
@@ -1,8 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsNumberString, IsOptional } from 'class-validator'
+import { ApiProperty, PartialType } from '@nestjs/swagger'
+import { IsEnum, IsNotEmpty, IsNumber, IsNumberString } from 'class-validator'
 import { Event } from '../event/event.entity'
 import { Ticket } from '../ticket/ticket.entity'
 import { User } from '../user/user.entity'
+import { GuestStatus } from './guest.entity'
 
 export class CreateGuestDto {
   @IsNotEmpty()
@@ -15,9 +16,28 @@ export class CreateGuestDto {
   ticket: Ticket
 }
 
-export class GetGuestDto {
-  @ApiProperty({ example: '1', description: 'Guest id', required: false })
-  @IsOptional()
+export class RequiredGuestDto {
+  @ApiProperty({ example: '1', description: 'Guest id', required: true })
+  @IsNotEmpty()
   @IsNumberString()
-  readonly id?: string
+  readonly id: string
+
+  @ApiProperty({ example: '1', description: 'Event id', required: true })
+  @IsNotEmpty()
+  @IsNumberString()
+  readonly eventId: string
+}
+
+export class GetGuestDto extends PartialType(RequiredGuestDto) {}
+
+export class ChangeGuestStatusDto {
+  @ApiProperty({ example: 1, description: 'Guest id', required: true })
+  @IsNotEmpty()
+  @IsNumber()
+  readonly id: number
+
+  @ApiProperty({ enum: GuestStatus, example: GuestStatus.Accepted, description: 'Guest status', required: true })
+  @IsEnum(GuestStatus)
+  @IsNotEmpty()
+  readonly status: GuestStatus
 }
