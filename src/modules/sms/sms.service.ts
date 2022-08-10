119a433cfd3135577dd81bbd0ee41bf711348a48
NarutoMinecrafter <www.iwanmayor.com@gmail.com>
2022-08-10 16:52:56 +0300

    remove sms

--- src/modules/sms/sms.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common'
-import { FirebaseAdmin, InjectFirebaseAdmin } from 'nestjs-firebase'
-
-@Injectable()
-export class SmsService {
-  constructor(@InjectFirebaseAdmin() private readonly firebase: FirebaseAdmin) {}
-
-  public sendCode() {}
-}
