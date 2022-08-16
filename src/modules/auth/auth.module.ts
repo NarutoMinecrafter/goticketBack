79d63e267bcb572f5701633496d9af944c5ca82a
NarutoMinecrafter <www.iwanmayor.com@gmail.com>
2022-08-16 17:39:07 +0300

    update notifications and test

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,13 +6,19 @@ import { AuthService } from './auth.service'
 import { AuthController } from './auth.controller'
 import { UserModule } from './../user/user.module'
 import { JwtStrategy } from './jwt.startegy'
+import { NotificationModule } from '../notification/notification.module'
 
 dotenv.config()
 
 const { JWT_SECRET } = process.env
 
 @Module({
-  imports: [JwtModule.register({ secret: JWT_SECRET, signOptions: { expiresIn: '7d' } }), PassportModule, UserModule],
+  imports: [
+    JwtModule.register({ secret: JWT_SECRET, signOptions: { expiresIn: '7d' } }),
+    PassportModule,
+    UserModule,
+    NotificationModule
+  ],
   providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
   exports: [JwtModule]
