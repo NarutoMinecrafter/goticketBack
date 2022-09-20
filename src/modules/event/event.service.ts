9e4d1968ad81bbcbbcf55e81ef7f6cceda69a8b9
NarutoMinecrafter <www.iwanmayor.com@gmail.com>
2022-09-20 14:04:15 +0300

    fix

--- src/modules/event/event.service.ts
+++ src/modules/event/event.service.ts
@@ -232,7 +232,7 @@ export class EventService {
       return 0
     })
 
-    return filteredEvents.filter(event => new Date(event.endDate) >= new Date())
+    return filteredEvents.filter(event => new Date(event.endDate) <= new Date())
   }
 
   getBy<T extends keyof Event>(
